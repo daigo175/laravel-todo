8860c56aa770ceef7a01004b521f8efbedf1b855
daigo175 <dt.happylife@gmail.com>
2024-11-07 11:53:32 +0900

    認証機能実装のために、breezeをインストール

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\FolderController;
@@ -7,6 +8,19 @@ use App\Http\Controllers\FolderController;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
 Route::controller(TaskController::class)->group(function () {
     Route::get('/folders/{id}/tasks', 'index')->name('tasks.index');
     Route::get('/folders/{id}/tasks/create', 'create')->name('tasks.create');
